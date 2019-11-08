5cbc5d0442125f2cc2b759794d2c1248b7d5c283
SaraMacri <sara.macri02@universitadipavia.it>
2019-11-08 16:36:43 +0100

    Riempimento servlet dei metodi per cercare determinate carte tramite filtri

--- src/servlets/SearchServlet.java
+++ src/servlets/SearchServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import collection.Card;
+import collection.CollectionOwn;
 import dao.CardsDao;
 import dao.CardsDaoImpl;
 import platform.Platform;
@@ -16,7 +17,7 @@ import java.util.Map;
 
 @WebServlet(name = "SearchServlet", urlPatterns = "/Search")
 public class SearchServlet extends AbstractServlet {
-    private String DEFAULT_ROUTE = "/views/search.jsp";
+    private String DEFAULT_ROUTE = "/views/userprofile.jsp";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -26,10 +27,40 @@ public class SearchServlet extends AbstractServlet {
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        forwardTo(request, response, INDEX_ROUTE);
+        try {
+            request.getSession().setAttribute("filter", null);
+            ArrayList<Card> filter = searchFilter(request);
+
+            if(filter!= null){
+                request.getSession().setAttribute("filter",filter);
+                response.sendRedirect(request.getContextPath()+DEFAULT_ROUTE);
+            }
+            else{
+                forwardTo(request, response, INDEXPROFILE_ROUTE);
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
+    private  ArrayList<Card> searchFilter(HttpServletRequest request) throws SQLException {
+        String filter = request.getParameter("filtersearch");
+        String search = request.getParameter("Filter");
+        CollectionOwn logged= (CollectionOwn) request.getSession().getAttribute("logged");
+        String username= logged.getOwner().getUsername();
+        Platform platform = Platform.getInstance();
+        switch (filter){
+            case "Category":
+                return platform.findByCategory(username,search);
 
+            case "Class":
+                return platform.findByClass(username,search);
+
+            case "Type":
+                return platform.findByType(username, search);
+        }
+        return null;
+    }
 
 }
 
