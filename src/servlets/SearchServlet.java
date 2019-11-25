7b11048d97f2ae7489bb58bce1c733bc0fda294f
SaraMacri <sara.macri02@universitadipavia.it>
2019-11-26 00:04:26 +0100

    Modifiche SearchServlet

--- src/servlets/SearchServlet.java
+++ src/servlets/SearchServlet.java
@@ -5,6 +5,8 @@ import collection.CollectionOwn;
 import dao.CardsDao;
 import dao.CardsDaoImpl;
 import platform.Platform;
+import userSide.Exchange;
+import userSide.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,10 +19,9 @@ import java.util.Map;
 
 @WebServlet(name = "SearchServlet", urlPatterns = "/Search")
 public class SearchServlet extends AbstractServlet {
-    private String DEFAULT_ROUTE = "/views/userprofile.jsp";
+    private String DEFAULT_ROUTE = "/views/homepage.jsp";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 
 
     }
@@ -29,32 +30,41 @@ public class SearchServlet extends AbstractServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
             request.getSession().setAttribute("filter", null);
-            ArrayList<Card> filter = searchFilter(request);
+            ArrayList<Exchange> filter = searchFilter(request);
 
-            if(filter!= null){
-                request.getSession().setAttribute("filter",filter);
-                response.sendRedirect(request.getContextPath()+DEFAULT_ROUTE);
-            }
-            else{
+            if (filter != null) {
+                request.getSession().setAttribute("filter", filter);
+                response.sendRedirect(request.getContextPath() + DEFAULT_ROUTE);
+            } else {
                 forwardTo(request, response, INDEXPROFILE_ROUTE);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private  ArrayList<Card> searchFilter(HttpServletRequest request) throws SQLException {
+    private ArrayList<Exchange> searchFilter(HttpServletRequest request) throws SQLException {
         String filterCategory = request.getParameter("filterCategory");
-        int category= Integer.parseInt(filterCategory);
-        String filterClass= request.getParameter("filterClass");
-        String filterType= request.getParameter("filterType");
-        String filterCard= request.getParameter("filterCard");
-        CollectionOwn logged= (CollectionOwn) request.getSession().getAttribute("logged");
-        String username= logged.getOwner().getUsername();
+        int category = Integer.parseInt(filterCategory);
+        String filterClass = request.getParameter("filterClass");
+        String filterType = request.getParameter("filterType");
+        String filterCard = request.getParameter("filterCard");
+        CollectionOwn logged = (CollectionOwn) request.getSession().getAttribute("logged");
+        User user = logged.getOwner();
         Platform platform = Platform.getInstance();
-        return null;
-    }
+        //serie di if/else per controllare che metodi dao siano giusti
+        if (category != 0) {
+            return platform.findTByCategoryCard(user, filterCategory);
+        } else if (filterClass != null) {
+            return platform.findTByClassCard(user, filterClass);
+        } else if (filterType != null) {
+            return platform.findTByTypeCard(user, filterType);
+        } else if (filterCard != null) {
+            return platform.findTByNameCard(user, filterCard);
+        } else
+            return null;
 
+    }
 }
 
 
