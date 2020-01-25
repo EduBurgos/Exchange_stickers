31a5c02bbf14d2689037ed5d423fbfc24818d430
SaraMacri <sara.macri02@universitadipavia.it>
2020-01-25 12:20:59 +0100

    tolto cardTest

--- Javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"collection"},{"l":"dao"},{"l":"platform"},{"l":"servlets"},{"l":"test"},{"l":"test.dao"},{"l":"userSide"}]
--- src/test/CardTest.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class CardTest {
-}
